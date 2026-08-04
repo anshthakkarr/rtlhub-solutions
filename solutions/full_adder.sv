5d9e281f285e1853c4abc8d928b2da499506ae1a
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-03 22:43:48 -0700

    Update RTLHub solution: full adder

--- solutions/full_adder.sv
+++ solutions/full_adder.sv
@@ -1,1 +1,18 @@
-1
+`default_nettype none 
+
+module adder (
+    input a, b, cin,
+    output sum, cout
+);
+
+logic ha1_sum, ha1_cout, ha2_sum, ha2_cout;
+
+assign ha1_sum = a ^ b;
+assign ha1_cout = a & b;
+assign ha2_sum = ha1_sum ^ cin;
+assign ha2_cout = cin & ha1_sum;
+assign sum = ha2_sum;
+assign cout = ha2_cout | ha1_cout;
+
+
+endmodule
