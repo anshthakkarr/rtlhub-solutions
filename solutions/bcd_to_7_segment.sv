381c5397f2268cdeaa5fa6dac288865cb7ecea63
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-03 22:44:35 -0700

    Add RTLHub solution: bcd to 7-segment

---
+++ solutions/bcd_to_7_segment.sv
@@ -0,0 +1,24 @@
+`default_nettype none
+
+module bcd_to_7seg (
+    input [3:0] bcd,
+    output logic [6:0] seg
+);
+
+always_comb begin
+    case (bcd)
+        4'h0: seg = 7'b1111110;
+        4'h1: seg = 7'b0110000;
+        4'h2: seg = 7'b1101101;
+        4'h3: seg = 7'b1111001;
+        4'h4: seg = 7'b0110011;
+        4'h5: seg = 7'b1011011;
+        4'h6: seg = 7'b1011111;
+        4'h7: seg = 7'b1110000;
+        4'h8: seg = 7'b1111111;
+        4'h9: seg = 7'b1111011;
+        default: seg = '0;
+    endcase
+end
+
+endmodule
