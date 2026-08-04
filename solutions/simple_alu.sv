ff04448dc6859ca77cf3fcb23787ce14adf2851b
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-03 22:43:56 -0700

    Update RTLHub solution: simple alu

--- solutions/simple_alu.sv
+++ solutions/simple_alu.sv
@@ -1,1 +1,58 @@
-1
+`default_nettype none
+
+module alu (
+    input [3:0] a, b,
+    input [2:0] op,
+    output logic [3:0] result,
+    output logic zero
+);
+
+always_comb begin
+    case (op)
+        3'b000: begin
+            result = a & b;
+            zero = ~(result == 4'd0) ? 1'b0: 1'b1;
+        end
+        3'b001: begin
+            result = a | b;
+            zero = ~(result == 4'd0) ? 1'b0: 1'b1;
+        end
+        3'b010: begin
+            result = a + b;
+            zero = ~(result == 4'd0) ? 1'b0: 1'b1;
+
+        end
+        3'b011: begin
+            result = a - b;
+            zero = ~(result == 4'd0) ? 1'b0: 1'b1;
+
+        end
+        3'b100: begin
+            result = a ^ b;
+            zero = ~(result == 4'd0) ? 1'b0: 1'b1;
+
+        end
+        3'b101: begin
+            result = (a < b) ? 1 : 0;
+            zero = ~(result == 4'd0) ? 1'b0: 1'b1;
+
+        end
+        3'b110: begin
+            result = ~(a | b);
+            zero = ~(result == 4'd0) ? 1'b0: 1'b1;
+
+        end
+        3'b111: begin
+            result = ~(a & b);
+            zero = ~(result == 4'd0) ? 1'b0: 1'b1;
+
+        end
+        default: begin
+            result = 4'b0000;
+            zero = 1'b1;
+        end
+    endcase
+
+end
+
+endmodule
