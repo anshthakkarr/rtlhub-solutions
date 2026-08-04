9381834f1c5ddd5568a641f1f8693acab9fbc476
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-03 22:44:04 -0700

    Update RTLHub solution: odd parity generator

--- solutions/odd_parity_generator.sv
+++ solutions/odd_parity_generator.sv
@@ -1,1 +1,22 @@
-1
+`default_nettype none 
+
+module odd_parity_mux (
+    input a, b, c,
+    output f
+);
+
+logic [7:0] mux_data;
+
+assign mux_data[0] = 1'b0;
+assign mux_data[1] = 1'b1;
+assign mux_data[2] = 1'b1;
+assign mux_data[3] = 1'b0;
+assign mux_data[4] = 1'b1;
+assign mux_data[5] = 1'b0;
+assign mux_data[6] = 1'b0;
+assign mux_data[7] = 1'b1;
+
+
+assign f = ^{a, b, c};
+
+endmodule
