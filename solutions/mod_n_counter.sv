afdddb1b189253e1da8d85f9871b4df534c1fe6e
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-03 22:45:13 -0700

    Update RTLHub solution: mod-n counter

--- solutions/mod_n_counter.sv
+++ solutions/mod_n_counter.sv
@@ -1,1 +1,17 @@
-1
+module mod_n_counter (
+    input clk, reset,
+    input [3:0] modulo,
+    output logic [3:0] count
+);
+
+always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+        count <= 4'h0;
+    end else if (count >= modulo - 1) begin
+            count <= 4'h0;
+    end else begin
+        count <= count + 1;
+    end
+end
+
+endmodule
