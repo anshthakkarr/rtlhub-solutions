b093871db684fa4679bb7141cac3d104fc640acf
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-03 22:43:12 -0700

    Update RTLHub solution: 4:1 multiplexer

--- solutions/4_1_multiplexer.sv
+++ solutions/4_1_multiplexer.sv
@@ -1,1 +1,12 @@
-1
+module mux (
+    input d0, d1, d2, d3, s0, s1,
+    output y
+);
+    logic mux1_out, mux2_out;
+
+    assign mux1_out = s0 ? d1 : d0;
+    assign mux2_out = s0 ? d3 : d2;
+
+    assign y = s1 ? mux2_out : mux1_out;
+
+endmodule
