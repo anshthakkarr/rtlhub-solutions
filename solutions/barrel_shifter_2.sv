5f1e3d0f44c769d04720bb75cadf6cad528d3225
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-08 13:02:15 -0700

    Add RTLHub solution: barrel shifter (2) (barrel_shifter.sv)

---
+++ solutions/barrel_shifter_2.sv
@@ -0,0 +1,17 @@
+module barrel_shifter (
+    input  [7:0] data_in,
+    input  [2:0] shift_bits,
+    input        left_right,
+    input        shift_mode,
+    output reg [7:0] data_out
+);
+
+    always @(*) begin
+        if (left_right) begin
+            data_out = shift_mode ? (data_in <<< shift_bits) : (data_in << shift_bits);
+        end else begin
+            data_out = shift_mode ? (data_in >>> shift_bits) : (data_in >> shift_bits);
+        end
+    end
+
+endmodule
