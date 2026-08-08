f143cad46659632fe556a85766ee22c17d0a5b26
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-07 20:05:50 -0700

    Add RTLHub solution: barrel shifter (3) (barrel_shifter_8bit.sv)

---
+++ solutions/barrel_shifter_3.sv
@@ -0,0 +1,10 @@
+module barrel_shifter_8bit (
+    input [7:0]         data_in,
+    input [2:0]         shift_bits,
+    input               left_right,
+    output logic [7:0]  data_out
+);
+
+assign data_out = left_right ? (data_in << shift_bits) : (data_in >> shift_bits);
+
+endmodule
