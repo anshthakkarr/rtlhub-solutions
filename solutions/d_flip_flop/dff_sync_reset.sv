3d88560b2939fd0c25283bfaf3cb6f7154b25eff
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-08 13:01:10 -0700

    Update RTLHub solution: d flip-flop (dff_sync_reset.sv)

--- solutions/d_flip_flop/dff_sync_reset.sv
+++ solutions/d_flip_flop/dff_sync_reset.sv
@@ -1,9 +1,9 @@
-module dff_sync_reset (
+module dff_async_reset (
     input clk, reset, d,
     output logic q
 );
 
-always_ff @(posedge clk) begin
+always_ff @(posedge clk or posedge reset) begin
     if (reset) begin 
         q <= 0;
     end else begin
