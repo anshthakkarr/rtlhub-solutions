995f87532b494eb98e9d7e21b6f3c896e232d91c
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-10 10:44:25 -0700

    Update RTLHub solution: johnson counter (johnson_counter.sv)

--- solutions/johnson_counter.sv
+++ solutions/johnson_counter.sv
@@ -1,19 +1,16 @@
-module johnson_counter (
+module mod_n_counter (
     input clk, reset,
+    input [3:0] modulo,
     output logic [3:0] count
 );
 
 always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
         count <= 4'h0;
+    end else if (count >= modulo - 1) begin
+            count <= 4'h0;
     end else begin
-        if (count == 4'h0) begin
-            count <= count + 1'b1;
-        end else if (count[3] == 0) begin
-            count <= {count[2:0], 1'b1};
-        end else begin
-            count <= count << 1;
-        end
+        count <= count + 1;
     end
 end
 
