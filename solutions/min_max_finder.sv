97580e8a63c6aa6f2626ce4af35e0b64cd7514e2
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-10 10:49:21 -0700

    Update RTLHub solution: min-max finder (min_max_finder.sv)

--- solutions/min_max_finder.sv
+++ solutions/min_max_finder.sv
@@ -1,58 +1,16 @@
-`default_nettype none
-
-module excess3_to_bcd (
-    input [3:0] excess3,
-    output logic [3:0] bcd,
-    output logic valid
+module min_max_finder (
+    input [3:0] a, b, c, d,
+    output logic [3:0] min, max
 );
 
-always_comb begin
-    case (excess3)
-        4'h3: begin 
-            bcd = 4'd0;
-            valid = 1'b1;
-        end
-        4'h4: begin 
-            bcd = 4'd1;
-            valid = 1'b1;
-        end
-        4'h5: begin 
-            bcd = 4'd2;
-            valid = 1'b1;
-        end
-        4'h6: begin 
-            bcd = 4'd3;
-            valid = 1'b1;
-        end
-        4'h7: begin 
-            bcd = 4'd4;
-            valid = 1'b1;
-        end
-        4'h8: begin 
-            bcd = 4'd5;
-            valid = 1'b1;
-        end
-        4'h9: begin 
-            bcd = 4'd6;
-            valid = 1'b1;
-        end
-        4'ha: begin 
-            bcd = 4'd7;
-            valid = 1'b1;
-        end
-        4'hb: begin 
-            bcd = 4'd8;
-            valid = 1'b1;
-        end
-        4'hc: begin 
-            bcd = 4'd9;
-            valid = 1'b1;
-        end
-        default: begin
-            bcd = 4'd0;
-            valid = 1'b0;
-        end
-    endcase
-end
+logic [3:0] inter_min_1, inter_min_2, inter_max_1, inter_max_2;
+
+assign inter_min_1 = (a <= d) ? a : d;
+assign inter_min_2 = (b <= c) ? b : c;
+assign inter_max_1 = (a >= d) ? a : d;
+assign inter_max_2 = (b >= c) ? b : c;
+
+assign min = inter_min_1 <= inter_min_2 ? inter_min_1 : inter_min_2;
+assign max = inter_max_1 >= inter_max_2 ? inter_max_1 : inter_max_2;
 
 endmodule
