9038e569e573db31826591a40f5d5bd32b76acab
Ansh Thakkar <ansh.thakkar@berkeley.edu>
2026-08-10 10:44:20 -0700

    Update RTLHub solution: up down counter (up_down_counter.sv)

--- solutions/up_down_counter.sv
+++ solutions/up_down_counter.sv
@@ -1,15 +1,14 @@
-module updown_counter (
-    input clk, reset, up_down,
-    output logic [3:0] count
+module dff_sync_reset (
+    input clk, reset, d,
+    output logic q
 );
 
-always_ff @(posedge clk or posedge reset) begin
-    if (reset) begin
-        count <= 4'h0;
-    end else if (up_down) begin
-        count <= count + 1;
+always_ff @(posedge clk) begin
+    if (reset) begin 
+        q <= 0;
     end else begin
-        count <= count - 1;
+        q <= d;
     end
-end
+end 
+
 endmodule
